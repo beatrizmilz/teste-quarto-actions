11761f564157cfbb3908d74855c5cb50bbf9a04a
Beatriz Milz <milz.bea@gmail.com>
2022-08-11 15:30:55 -0300

    quarto website

--- code.R
+++ code.R
@@ -1,5 +1,6 @@
+# criar quarto project com website
 usethis::use_git()
 usethis::use_github()
 usethis::use_github_pages()
-# criar arquivo index.qmd
+# criar arquivo index.qmd (ou renomear o existente)
 # criei o arquivo .github/workflows/quarto-publish.yml
